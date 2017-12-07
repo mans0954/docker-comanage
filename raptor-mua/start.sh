4216e7df9dd520ae88d3a2dba9fcf79414fc2146
Christopher Hoskin <christopher.hoskin@it.ox.ac.uk>
2017-12-07 15:44:17 +0000

    Revert "Add raptor-mua and raptor-web"
    This reverts commit bdf521d3f5e55a0dbb3a903b2c9ea33900f635df.

--- raptor-mua/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-until [ -f /opt/raptor/web/keys/raptor-web-public.crt ]
-do
-     sleep 5
-     echo "Waiting for Raptor web to come up"
-done
-keytool -import -noprompt -keystore /opt/raptor/mua/keys/authorised-keys.jks -storepass changeit -alias raptorweb -file /opt/raptor/web/keys/raptor-web-public.crt
-/etc/init.d/raptormuad console
