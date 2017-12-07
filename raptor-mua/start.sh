59eafcf67cc3e7b618a3548dbba701fbc3e9f6e4
Christopher Hoskin <christopher.hoskin@it.ox.ac.uk>
2017-12-07 15:44:03 +0000

    Revert "Raptor ICA (not quite working yet)"
    This reverts commit 27a9cad894f0333cbf5f12304410cbcb664afa59.

--- raptor-mua/start.sh
+++ raptor-mua/start.sh
@@ -6,13 +6,4 @@ do
      echo "Waiting for Raptor web to come up"
 done
 keytool -import -noprompt -keystore /opt/raptor/mua/keys/authorised-keys.jks -storepass changeit -alias raptorweb -file /opt/raptor/web/keys/raptor-web-public.crt
-
-until [ -f /opt/raptor/bar-ica/keys/raptor-ica-public.crt ]
-do
-     sleep 5
-     echo "Waiting for Raptor web to come up"
-done
-
-keytool -import -keystore /opt/raptor/mua/keys/authorised-keys.jks -storepass changeit -alias raptorica -file /opt/raptor/bar-ica/keys/raptor-ica-public.crt
-
 /etc/init.d/raptormuad console
