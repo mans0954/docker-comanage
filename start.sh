2abbd396905e02d243c2226f4f0fa2a6b11dd16e
Christopher Hoskin <mans0954@debian.org>
2018-01-21 16:18:16 +0000

    Database name and username may not match!

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until PGPASSWORD=${DB_PASSWORD} psql -h ${DB_HOST} -U ${DB_USER} -c '\l'; do
+until PGPASSWORD=${DB_PASSWORD} psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
